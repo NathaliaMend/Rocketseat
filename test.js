5f4a846ff56b4adc847fe7919929cf6bf4cbef4f
NathaliaMend <117854397+NathaliaMend@users.noreply.github.com>
2023-01-24 19:01:40 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+function viajar(transporte, embarcar) {
+    console.log()
+}
+
+const embarcar = () => {
+    console.log('mostrar documentos')
+    console.log('passar pela porta do trem')
+}
+
+viajar('transporte', embarcar)
