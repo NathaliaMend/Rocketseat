96cc50588b693b9f87c5b7508c0d9e941d965b71
NathaliaMend <117854397+NathaliaMend@users.noreply.github.com>
2023-01-24 12:50:26 -0300

    criei novos arquivos da aula sobre funcoes

---
+++ Functions/Functions_1.js
@@ -0,0 +1,28 @@
+// criar um aplicativo de frases motivacionais
+/*
+console.log('Estudar é muito bom')
+console.log('Paciência e persistência')
+console.log('Revisão é mãe do aprendizado')
+*/
+
+
+// É possível repetir o código usando funções
+// declaration -- declaração da função
+// function statement
+
+function createPHrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// executar a função
+//(outros nomes além de executar: rodar, chamar, invocar, execute, run, call, invoke )
+createPHrases()
+createPHrases()
+createPHrases()
+createPHrases()
+
+console.log('fim do programa')
+
+//RESUME: A Função serve para: agrupamento de código, reutilização do código quantas vezes eu quiser, dar significado (dar nome ao agrupamento, explicando o que o código vai fazer)e deixa tudo mais organizado também.
---
+++ Functions/functions.js
@@ -0,0 +1,12 @@
+// É possível declarar funções dentro de variáveis
+// function expression
+// function anonymous
+
+// parâmetros (parameters) da função / serve para receber os valores
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // 2 e 3 são arguments (argumentos) de uma função executada, porém função CRIADA são parâmetros.
+
+// RESUME : Aprendi a passar argumentos para a invocação de uma função e a criar os parâmetros que vão receber os argumentos da função.
