b86cf28289ffaffc6ca9f35f5896248b9da7f06d
NathaliaMend <117854397+NathaliaMend@users.noreply.github.com>
2023-01-24 13:33:36 -0300

    Assisti a aula"Retornando valores dentro da função"

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,6 +1,7 @@
 // É possível declarar funções dentro de variáveis
 // function expression
 // function anonymous
+// https://playcode.io/1097684
 
 // parâmetros (parameters) da função / serve para receber os valores
 const sum = function(number1, number2) {
@@ -10,3 +11,8 @@ const sum = function(number1, number2) {
 sum(2, 3) // 2 e 3 são arguments (argumentos) de uma função executada, porém função CRIADA são parâmetros.
 
 // RESUME : Aprendi a passar argumentos para a invocação de uma função e a criar os parâmetros que vão receber os argumentos da função.
+
+// reutilizando o bloco de códigos
+
+sum(4, 5)
+sum(56, 3587245029)
