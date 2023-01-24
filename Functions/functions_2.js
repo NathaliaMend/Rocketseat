54f8f174472d499841de81d90cf0e79b6ed4e8c9
NathaliaMend <117854397+NathaliaMend@users.noreply.github.com>
2023-01-24 17:39:05 -0300

    MENTORIA DE FUNÇÃO JUNTAMENTE A AULA CALLBACK FUNCTION

--- Functions/functions_2.js
+++ Functions/functions_2.js
@@ -33,6 +33,9 @@ console.log(total)
 
 
 
+
+
+
 // A MÉDIA DE DUAS PROVAS
 const média = function(prova1, prova2){
     let total = (prova1 + prova2) / 2
@@ -50,6 +53,8 @@ console.log(`a media dos três numeros é ${media(10, 23, 78)}`)
 
 
 
+
+
 // FUNÇÃO É UM LIQUIDIFICADOR
 
 function fazerSuco(fruta1, fruta2) {
@@ -59,3 +64,102 @@ function fazerSuco(fruta1, fruta2) {
 const copo = fazerSuco('banana', 'maçã')
 
 console.log(copo)
+
+
+
+
+
+
+// function scope
+
+let subject = 'create video'
+
+function createThink(subject){
+   subject = 'study'
+    return subject
+}
+console.log(subject)
+console.log(createThink(subject)) //?????????????????????
+
+
+
+
+
+
+
+// function hoisting (const/var)  ELEVAÇÃO
+
+sayMyName();
+
+function sayMyName(){
+    console.log('nathalia')
+
+}
+
+
+
+
+
+
+// ARROW FUNCTION / jeito mais curto de escrever uma função
+
+//const sayMyName = () => {}
+
+const sayMyName = (name) => {
+    console.log(name)
+}
+
+sayMyName('nath')
+
+
+
+
+
+// callback function (callback significa "chamar de volta") / uma função que esta passando como parametro para outra função
+
+function sayMyName(name){
+    console.log('antes de executar a função callback')
+
+    name()
+
+    console.log('depois de executar a callback')
+}
+
+sayMyName(
+    () => {
+        console.log('estou em uma callback')
+    }
+)
+
+
+
+
+
+
+
+
+// RECEITA DE SUCO
+
+
+
+function Suco(liquido, flocos, colocarfruta, recipiente){
+    colocarfruta()
+    console.log('Acrescentar 10g ' + flocos + '.')
+    console.log(`Acrescentar 300ml ${liquido}`)
+    recipiente()  
+ }
+
+const Morango= () => { console.log('suco de morango:') 
+                      console.log('lavar morango')
+                      console.log('tirar as folhinhas do morango')}
+
+const Banana = () => { console.log('suco de banana:')
+                       console.log('descasque a banana')
+                      console.log('pique a banana')}
+
+const copo = () => console.log('copo de vidro 500ml')
+const vasilha = () => console.log('vasilha de plástico de 500ml')
+
+Suco('leite', 'flocos de arroz', Morango, copo)
+console.log('--------------------')
+Suco('agua', 'flocos de aveia', Banana, vasilha)
