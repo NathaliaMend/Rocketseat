390796f4a14df8dae7ee19e0183150d7dfafa56d
NathaliaMend <117854397+NathaliaMend@users.noreply.github.com>
2023-01-24 14:12:30 -0300

    mentoria sobre arrow function

--- Functions/functions_2.js
+++ Functions/functions_2.js
@@ -13,3 +13,37 @@ console.log(`o número 1 é ${number1}`)
 console.log(`o número 2 é ${number2}`)
 console.log(`a soma é ${sum(number1, number2)}`)
 console.log(total)
+
+
+let total=5
+
+const times = function(n1, number2) {
+    let total = n1 * number2
+    return total
+}
+
+
+let number1 = 34
+let number2 = 25
+
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 2 é ${number2}`)
+console.log(`a multiplicação é ${times(number1, number2)}`)
+console.log(total)
+
+
+
+// A MÉDIA DE DUAS PROVAS
+const média = function(prova1, prova2){
+    let total = (prova1 + prova2) / 2
+    return total }
+  let nathalia = 7.5
+  let miguel = 9
+  console.log(`a média das provas é ${média(nathalia, miguel)}`)
+
+
+  
+
+//  ARROW FUCTION : CALCULANDO A MEDIA DE TRÊS NUMEROS
+  const media = (n1, n2, n3) => (n1 + n2 + n3) / 3
+console.log(`a media dos três numeros é ${media(10, 23, 78)}`)
