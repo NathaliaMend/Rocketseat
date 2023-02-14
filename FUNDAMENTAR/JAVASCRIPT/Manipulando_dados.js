6e82e7a2477b9fd7d1fe0c052c9ee1c7607b716b
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-02-14 16:49:32 -0300

    novas aulas

--- FUNDAMENTAR/JAVASCRIPT/Manipulando_dados.js
+++ FUNDAMENTAR/JAVASCRIPT/Manipulando_dados.js
@@ -25,3 +25,53 @@ console.log('9' + 5)  //O JavaScript ira imprimir "95" usando o TYPE COERSION
 
 console.log(number('9') + 5) //Foi usado, de maneira manual, uma funcao que converte a string em number para que a impressao seja "14". Entretanto, o JavaScript tambem pode usar esse typecasting em alguns momentos.
 
+
+
+
+// Manipulando strings e numeros
+
+// Contar quantos caracteres tem uma palavra e quantos digitos tem um numero.
+
+let word = "paralelepipedo"
+console.log(word.length)
+
+
+let number = 1234
+console.log(string(number).length)
+
+
+// Transformar um numero quebrado com 2 casas decimais 
+
+let number = 343.434322
+console.log(number.toFixed(2))   // O toFixed `e uma funcao que esta atrelada ao objeto 'number'. Quando uma funcao `e atrelada a um objeto, nos chamamos tambem de "metodo"/'method'. Servira para mostrar o numero de casas decimais que eu gostaria de ver. Ex: (2) . Quando se coloca o toFixed ira retornar uma string e com isso `e possivel trocar um ponto por virgula, o que nao acontece se eu quiser transformar essa string em numero.
+
+//TROCAR PONTO POR VIRGULA / '.replace("" , "")'
+// `E possivel atrelar duas funcoes ao mesmo tempo usando um ponto
+console.log(number.toFixed(2).replace("." , ","))
+
+
+
+
+
+// Transformar letras maiusculas em minusculas e o contrario tambem
+
+let word = "Programar e muito bacana!"
+console.log(word.toUppenCase())
+//o contrario `e:
+console.log(word.toLowerCase())
+
+
+
+// MANIPULADO STRINGS E ARRAYS
+
+
+// Separe um texto que contem espacos, em um novo array onde cada texto `e uma posicao do array. 
+
+let phrase = "Eu quero viver o Amor!"
+let myArray = phrase.split(" ") //o split serve para indicar o que eu quero separar no texto
+console.log(myArray)
+
+//Depois disso, transforme o array em um texto e onde eram espacos, coloque _
+let phraseWithUnderScore = myArray.join("_") // o metodo 'join' serve para indicar com o que voce quer juntar o texto.
+console.log(phraseWithUnderScore) // posso ainda acrescentar o que aprendi a cima. Ex:
+console.log(phraseWithUnderScore.toLowerCase())
