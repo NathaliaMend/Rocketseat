d59ecee04a787b96794613903e8659618e342a59
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-02-20 16:21:56 -0300

    NOVA AULA DE EXPRESSOES E OPERADORES

---
+++ FUNDAMENTAR/JAVASCRIPT/Expressoes_e_Operadores.js
@@ -0,0 +1,10 @@
+/* Expressoes (qualquer linha de codigo que resolve alguma coisa) e Operadores
+
+   -Expression (ex.: let Number = 1)
+   -Operators
+       binary (ex.: console.log(number + 1), chamdo operator binary porque `e preciso de dois valores para que o operador esteja entre eles, ou seja, o sinal de mais/+)
+       unary (ex.: console.log(++number), quando so ha um valor e `e usado um operador junto.)
+       ternary (s`o ha um caso de ternary (ex.: console.log(true ? 'alo' : 'nada')/traduzindo...:Se for verdade, eu quero 'alo', se for falso quero 'nada'/)
+*/
+// VEREMOS VARIOS TIPOS DE OPERADORES E EXPRESSOES AGORA..
+
