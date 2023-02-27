8a160df163e274e081b2735097278c4a32a0ec2b
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-02-27 18:39:55 -0300

    nova aula de comparacao e diferenca

---
+++ FUNDAMENTAR/JAVASCRIPT/Expressoes e operadores/Operadores de comparacao.js
@@ -0,0 +1,14 @@
+// Operadores de comparacao
+
+// Ira` compararvalores e retornar um Boolean como resposta `a comparacao
+let one = 1
+let two = 2
+
+// ==  significa: igual a ...
+// console.log( two == 1)
+// console.log( one == "1")
+
+// != significa: diferente de...
+// console.log( one != "1")
+// console.log( one != 1)
+// console.log( one != two)
