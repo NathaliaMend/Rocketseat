3287cc62e9a62e76055408ef69f6c6860414cde8
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-03-15 18:56:02 -0300

    aula de operadores de comparacao (MAIOR MENOR (IGUAL) )

--- FUNDAMENTAR/JAVASCRIPT/Expressoes e operadores/Operadores de comparacao.js
+++ FUNDAMENTAR/JAVASCRIPT/Expressoes e operadores/Operadores de comparacao.js
@@ -21,3 +21,20 @@ console.log( one === 1 )
 //  !== estritamente diferente de
 console.log( two !== "2")
 console.log( two !== 2 )
+
+
+
+// > Maior que 
+console.log(one > two)
+
+// >= Maior igual
+console.log( one >= 1)
+console.log( two >= 1)
+
+// < Menor que
+console.log(one < two)
+
+// <= Menor igual a 
+console.log( one <= two)
+console.log( one <= 1)
+console.log( one <= 0)
