2232f989c7dbc4448b2ff0a89642b56a75f714be
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-03-16 15:34:48 -0300

    Aula sobre Assignment ( operadores de atribuicao)

---
+++ FUNDAMENTAR/JAVASCRIPT/Expressoes e operadores/Operadores_de_atribuicao.js
@@ -0,0 +1,25 @@
+// Operadores de atribuicao ( Assignment )
+let x
+
+//assignment
+x = 1 // o sinal de igual significa que o x RECEBE o valor 1.
+console.log(x)
+
+// addition assignment
+x += 2 // Essa expressao significa x = x+2 . Entao o x receber`a o valor de x+2, como temos o valor de x `a cima como 1, a expressao tera o resultado de (x = 1+2), com isso o console.log imprimir`a o valor 3.
+
+// subtraction assignment
+x -= 1 // x = x-1
+ 
+// multiplication assignment
+x *= 2 // x = x*2
+
+// division assignment
+x /= 2 // x = x/2
+
+// remainder (resto) 
+x %= 2 // x = x%2
+
+// exponetiation
+x **= 2 // x = x**2
+
