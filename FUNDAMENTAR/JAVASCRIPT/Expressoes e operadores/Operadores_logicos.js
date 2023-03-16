31ab1d1c1975cdfb5a078f1d2b38ab5f426fc7ea
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-03-16 16:06:41 -0300

    Aula sobre logical operators / Operadores logicos

---
+++ FUNDAMENTAR/JAVASCRIPT/Expressoes e operadores/Operadores_logicos.js
@@ -0,0 +1,16 @@
+// Operadores logicos ( logical operators)
+
+// - 2 valores booleanos, quando verificados, resultara em verdadeiro ou falso
+
+let pao = true
+let queijo = true
+
+// AND &&  ( o console.log imprimira true somente se todos os atributos forem verdadeiros/true)( o console.log imprime false quando um ou mais atributos tiverem valor false )
+console.log(pao && queijo)
+
+// OR || (qualquer um que for verdadeiro, o console.log imprime true)( o console.log imprimira false somente se todos os atributos forem falsos/false)
+console.log(pao || queijo)
+
+// NOT ! (Negar/Negacao)(o simbolo de negacao serve para trocar o valor do booleano)(ele fica semanticamente antes do atributo)
+console.log(!pao)
+ 
