6481028419111fcb5c9c69afb2ac94368dc33b3d
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-03-22 11:52:53 -0300

    nova aula sobre operador de strings

---
+++ FUNDAMENTAR/JAVASCRIPT/Expressoes e operadores/Operadores_para_string.js
@@ -0,0 +1,14 @@
+// Operador de strings (String operator)
+
+// comparison (comparacao)
+// console.log( 'a' == 'a')
+
+// concatenation (concatenacao) . Retorna a uniao de duas ou mais strings:
+let alpha = 'alpha'
+alpha += 'bet'
+console.log( alpha + 'bet' + 's')
+
+
+
+
+
