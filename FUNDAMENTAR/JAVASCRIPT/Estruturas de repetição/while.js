d0a047d03e9142dd23b36a449fbe980d55703e20
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-04-19 16:13:22 -0300

    aula sobre estrutura de repeticao WHILE

---
+++ FUNDAMENTAR/JAVASCRIPT/Estruturas de repetição/while.js
@@ -0,0 +1,24 @@
+// Estrutura de repeticao
+// While (Usar o while ou o for? R: Usa o while quando nao se sabe exatamente o momento da parada)
+
+
+
+while(true){  }  // Aqui o While nunca vai parar 
+
+///////////////////////////////////////////////////////
+
+let i = 0;
+while(i < 10) {
+    console.log(i)
+
+    i++;   // altera o valor da condicao de parada
+}
+
+///////////////////////////////////////////////////////
+
+let i = 454592203079588
+while (i > 10) {
+    console.log (i)
+
+    i/=35;
+}
