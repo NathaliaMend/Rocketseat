112a30cf150f1112a3a1f900118744cc06267287
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-04-19 18:21:04 -0300

    Exercicio proposto pelo curso

---
+++ FUNDAMENTAR/JAVASCRIPT/EXERCICIOS/Sistemas_de_Notas_Escolares.js
@@ -0,0 +1,12 @@
+/* Transformar notas escolares
+
+Crie um algoritimo que transforme as notas do sistema numérico para sitema de notas em caracteres tipo A B C
+
+* De 90 para cima- A
+* De 80 a 89  -    B
+* De 70 a 79  -    C
+* De 60 a 69  -    D
+* Menor que 60 -   F
+
+*/
+
