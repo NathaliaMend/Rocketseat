7bedeef3978526b6bdc388487fea8f1d1a59d2d1
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-04-19 17:18:57 -0300

    Nova aula sobre For.. of

---
+++ FUNDAMENTAR/JAVASCRIPT/Estruturas de repetição/For...Of.js
@@ -0,0 +1,15 @@
+// For...of  (é uma declaração que criará um looping também através de alguma variável previamente colocada, podendo ser uma variável do tipo string ou não)
+// For of serve tanto para strings quanto para arrays.
+
+let name = 'Nath'
+
+for(let char of name){// pegue um caracter (char) do (of) name (nome)  
+        console.log(char)   
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+let names = ['Miguel', 'Jose', 'Nicole']
+
+for (let name of names){
+    console.log(name)
+}// imprimirá os elementos do array um por um . (formando uma lista com esses nomes)
