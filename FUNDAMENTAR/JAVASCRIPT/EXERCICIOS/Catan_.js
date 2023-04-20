2660dc4f064454cc76cd808b25bad96fcaa6ae1a
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-04-20 18:57:44 -0300

    primeira parte do exercicio proposto pelo mentor miguel Feito

---
+++ FUNDAMENTAR/JAVASCRIPT/EXERCICIOS/Catan_.js
@@ -0,0 +1,26 @@
+/* Dado um conjunto de cartas com a quantidade de cada uma, podendo ser (madeira, argila, pedra, palha, ovelha) precisa retornar se pode ou nao construir uma cidade [a cidade é constituída por: Madeira, argila, Ovelha e palha] uma de cada */
+
+const cidade = {
+    madeira: 1,
+    argila: 1,
+    ovelha: 1,
+    palha: 1
+}
+
+           function construa (mardeira, argila, ovelha, palha) {
+                if (tenho != cidade) {
+                    return 'É possível construir uma ou mais Cidades'
+                } else {
+                    return 'Não há recursos suficientes para criar uma cidade'
+                }
+            } 
+
+
+let tenho = { madeira: 0,
+    argila: 0,
+    ovelha: 0,
+    palha: 0,
+    pedra: 0 
+    }
+
+construa(tenho)
