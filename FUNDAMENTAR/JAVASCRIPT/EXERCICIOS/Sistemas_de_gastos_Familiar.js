ef31a37373856af03bc3c7219ed6f6409614b371
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-04-20 18:56:35 -0300

    Exercicio proposto rocketseat 2

---
+++ FUNDAMENTAR/JAVASCRIPT/EXERCICIOS/Sistemas_de_gastos_Familiar.js
@@ -0,0 +1,6 @@
+/* ### Sistema de gastos familiar
+
+    Crie um objeto que possuirá duas propriedades, ambas do tipo array:
+            * receitas []
+            * despesas []
+    Agora crie uma função que irá calcular o total de receitas e despesas e irá mostrar uma mensagem se a família está com o saldo positivo ou negativo, seguido do valor do saldo */
