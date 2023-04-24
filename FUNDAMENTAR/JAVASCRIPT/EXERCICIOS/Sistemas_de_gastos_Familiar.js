72f3d5561ebaca9cbc3a41370f3fe6083e181c09
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-04-24 18:43:36 -0300

    Adicionei a funcao feita pelo prof

--- FUNDAMENTAR/JAVASCRIPT/EXERCICIOS/Sistemas_de_gastos_Familiar.js
+++ FUNDAMENTAR/JAVASCRIPT/EXERCICIOS/Sistemas_de_gastos_Familiar.js
@@ -73,4 +73,34 @@ function financas (contas) {
                 return 'Saldo positivo'
         } else { return 'Saldo zerado'}
 }
+//////////////////////////////////////////////////////////////////////////////////
+// Do Professor:
 
+let family = {
+        incomes: [ 2500, 3200, 250.43, 360.45],
+        expenses: [ 320.24, 128.45, 176.87, 1450.00]
+}
+function sum(array) {
+        let total = 0;
+       
+  for(let value of array) {
+        total += array
+  }  return total
+}
+
+function calculeteBalance() {
+        const calculeteIncomes = sum(family.incomes)
+        const calculeteExpenses = sum(family.expenses)
+
+        const total = calculeteIncomes - calculeteExpenses
+
+        const itsOk = total >= 0
+
+        let balanceText = "Negativo"
+   if(itsOk) {
+        balanceText = "Positivo"
+   }
+   console.log(`Seu saldo é ${balanceText}: ${total.toFixed(2)}`)
+} 
+
+calculeteBalance()
