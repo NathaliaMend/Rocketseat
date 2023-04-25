468243a8dccdc496a8e7797e58676e8d5a7f1447
Nathália <117854397+NathaliaMend@users.noreply.github.com>
2023-04-25 10:13:17 -0300

    Completei o exercicio sozinha :D. Finalizacao com dicas do mentor miguel.

--- FUNDAMENTAR/JAVASCRIPT/EXERCICIOS/Celsius_Fahrenheit.js
+++ FUNDAMENTAR/JAVASCRIPT/EXERCICIOS/Celsius_Fahrenheit.js
@@ -7,3 +7,15 @@ F = C * 9/5 + 32
 
 */
 
+
+function FahrenheitParaCelsius(fahrenheit) {
+    const celsius = (fahrenheit - 32) * 5/9 
+    return `${celsius.toFixed(1)}°C`
+}
+
+function CelsiusParaFahrenheit(celsius) {
+    const fahrenheit = celsius * 9/5 + 32
+   return `${fahrenheit.toFixed(1)}°F`
+}
+
+
