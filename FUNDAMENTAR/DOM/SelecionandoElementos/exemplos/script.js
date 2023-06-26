78c3c5ab27cfad7b36ab74c2efb1495c659774d8
NathaliaMend <nathaliamend017@gmail.com>
2023-06-26 16:23:45 -0300

    get element na pratica

---
+++ FUNDAMENTAR/DOM/SelecionandoElementos/exemplos/script.js
@@ -0,0 +1,35 @@
+// getElementById() --> Serve para pegar/selecionar o elemento
+//const element = document.getElementById('blog-title')
+//console.log(element)
+
+
+// .getElementsByClassName() --> Serve para selecionar uma ou mais classes.
+//const element = document.getElementsByClassName('one')
+//console.log(element)
+
+
+//.getElementsByTagName() --> Serve para selecionar o elemento pelo nome da tag
+//const element=document.getElementsByTagName('meta')
+//console.log(element)
+
+
+// .querySelector() --> Serve para selecionar um elemento dentre diversos possiveis selectors. selecionará o primeiro selector.
+// const element = document.querySelector('.one')
+// OU 
+//const element = document.querySelector('[src]')
+// OU
+//const element = document.querySelector('meta')
+// OU
+//const element = document.querySelector('#blog-title')
+//console.log(element)
+
+
+//.querySelectorAll() --> Serve para selecionar varios elementos entre diversos possiveis selectors . retorna um nodelist 
+const element = document.querySelectorAll('.one')
+console.log(element)
+
+
+
+
+
+
